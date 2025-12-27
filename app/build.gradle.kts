2076f97deabb44997d5dcc6b9a24662cb7c26df0
TiamCow <23162028@student.hcmute.edu.vn>
2025-12-27 16:10:37 +0700

    public playlist

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -41,4 +41,8 @@ dependencies {
     testImplementation(libs.junit)
     androidTestImplementation(libs.ext.junit)
     androidTestImplementation(libs.espresso.core)
+    implementation("androidx.appcompat:appcompat:1.6.1")
+    implementation("com.google.android.material:material:1.9.0")
+    implementation("androidx.constraintlayout:constraintlayout:2.1.4")
+    implementation("com.github.bumptech.glide:glide:4.15.1")
 }
