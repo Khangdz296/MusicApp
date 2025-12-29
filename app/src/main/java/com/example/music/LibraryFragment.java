918f55aa6576c538b5ccab7c5f5172cab14375b1
Duy Hoang <23162025@student.hcmute.edu.vn>
2025-12-29 10:16:34 +0700

    Tao giao dien library

---
+++ app/src/main/java/com/example/music/LibraryFragment.java
@@ -0,0 +1,20 @@
+package com.example.music;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+public class LibraryFragment extends Fragment {
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        // Dòng này quan trọng nhất: Nó kết nối file Java này với file giao diện XML bạn đã vẽ
+        // R.layout.fragment_library phải trùng tên với file XML của bạn
+        return inflater.inflate(R.layout.fragment_library_hoang, container, false);
+    }
+}
