14033951d039b65e82a4d7490bc3a0ace926778d
Huy <nhathuycop@gmail.com>
2026-01-06 21:47:32 +0700

    add background music

--- app/src/main/java/com/example/music/api/RetrofitClient.java
+++ app/src/main/java/com/example/music/api/RetrofitClient.java
@@ -11,7 +11,7 @@ public class RetrofitClient {
     public static Retrofit getClient() {
         if (retrofit == null) {
             retrofit = new Retrofit.Builder()
-                    .baseUrl("http://10.0.2.2:8080/")
+                    .baseUrl("http://192.168.1.10:8080/")
                     .addConverterFactory(GsonConverterFactory.create())
                     .build();
         }
